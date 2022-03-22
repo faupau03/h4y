4440bdb6cc96c9ba20d369638283a57f0ca1d672
Paul Paffe <paul.paffe@gmx.de>
2022-03-22 13:25:18 +0100

    build

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/h4y/sw.js', { scope: '/h4y/' })})}
