5bfa5843ebda8d615b6e213d1fcf00d1082be68a
Paul Paffe <paul.paffe@gmx.de>
2022-03-24 14:44:01 +0100

    update meta tags

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/h4y/',
+  base: '/',
   plugins: [
     vue(),
     VitePWA({
