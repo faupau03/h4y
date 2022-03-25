fbb9e5f4e922c1a850da667d849ae80d88dec397
Paul Paffe <paul.paffe@gmx.de>
2022-03-25 19:51:02 +0100

    split components up into helper components Match, MatchLoading, Table, TableLoading

--- src/components/Club.vue
+++ src/components/Club.vue
@@ -187,6 +187,11 @@
                             <div id="league-info">
                                 <!-- Information about how many games and button to team component -->
                             </div>
+                            <MatchVue
+                                v-for="match in teamMatches"
+                                :key="match.gID"
+                                :match="match"
+                            />
                             <div id="no-data" v-show="!teamMatches.length">
                                 <div id="no-future-matches" v-show="!showAll">
                                     Keine zukünftigen Spiele
@@ -195,82 +200,7 @@
                                     Keine Spiele in dieser Klasse
                                 </div>
                             </div>
-                            <div
-                                v-for="match in teamMatches"
-                                :key="match.gID"
-                                class="rounded-lg bg-indigo-200 my-1 p-2 justify-between px-5 hover:scale-95 scale-100 transition-all"
-                            >
-                                <div
-                                    class="grid grid-cols-3 sm:grid-cols-6 gap-1"
-                                >
-                                    <div
-                                        id="teams"
-                                        class="text-black w-fit col-span-2 text-xs font-bold"
-                                    >
-                                        <!-- teams -->
-                                        {{ match.gHomeTeam }} :
-                                        {{ match.gGuestTeam }}
-                                    </div>
-
-                                    <div
-                                        id="date-time"
-                                        class="w-fit text-xs text-gray-800"
-                                    >
-                                        <div class="flex">
-                                            {{ match.gWDay }}
-                                            {{ match.gDate }}
-                                        </div>
-                                        <div class="flex">
-                                            <ClockIcon
-                                                class="h-4 mr-1 text-indigo-900"
-                                            />
-                                            {{ match.gTime }}
-                                        </div>
-                                    </div>
-
-                                    <div
-                                        id="location"
-                                        class="flex text-black col-span-2"
-                                    >
-                                        <!-- Gymnasium -->
-                                        <LocationMarkerIcon
-                                            class="h-4 w-4 text-indigo-900 shrink-0"
-                                        />
-                                        <span class="truncate text-xs">
-                                            {{ match.gGymnasiumName }}
-                                        </span>
-                                    </div>
-
-                                    <div
-                                        id="score"
-                                        class="flex w-fit text-black"
-                                    >
-                                        <div
-                                            class="min-w-[20px] mr-1 text-right"
-                                        >
-                                            {{ match.gHomeGoals }}
-                                        </div>
-                                        :
-                                        <div class="min-w-[20px] ml-1">
-                                            {{ match.gGuestGoals }}
-                                        </div>
-                                    </div>
-                                    <div
-                                        id="info"
-                                        class="col-span-3 text-xs text-gray-600"
-                                        :class="
-                                            match.gComment.length > 1
-                                                ? 'flex'
-                                                : 'hidden'
-                                        "
-                                    >
-                                        <InformationCircleIcon
-                                            class="h-4 w-4 mr-1"
-                                        />
-                                        {{ match.gComment }}
-                                    </div>
-                                </div>
-                            </div>
+                            
                         </div>
                     </DisclosurePanel>
                     <button
@@ -299,6 +229,8 @@ import {
 } from "@heroicons/vue/solid";
 import { StarIcon as StarIconOutline, ShareIcon } from "@heroicons/vue/outline";
 import { Disclosure, DisclosureButton, DisclosurePanel } from "@headlessui/vue";
+
+import MatchVue from "./Match.vue";
 
 const route = useRoute();
 const club = ref({});
---
+++ src/components/helpers/Match.vue
@@ -0,0 +1,66 @@
+<template>
+    <div
+        class="rounded-lg bg-indigo-200 my-1 p-2 justify-between px-5 hover:scale-95 scale-100 transition-all"
+    >
+        <div class="grid grid-cols-3 sm:grid-cols-6 gap-1">
+            <div
+                id="teams"
+                class="text-black w-fit col-span-2 text-xs font-bold"
+            >
+                <!-- teams -->
+                {{ match.gHomeTeam }} :
+                {{ match.gGuestTeam }}
+            </div>
+
+            <div id="date-time" class="w-fit text-xs text-gray-800">
+                <div class="flex">
+                    {{ match.gWDay }}
+                    {{ match.gDate }}
+                </div>
+                <div class="flex">
+                    <ClockIcon class="h-4 mr-1 text-indigo-900" />
+                    {{ match.gTime }}
+                </div>
+            </div>
+
+            <div id="location" class="flex text-black col-span-2">
+                <!-- Gymnasium -->
+                <LocationMarkerIcon class="h-4 w-4 text-indigo-900 shrink-0" />
+                <span class="truncate text-xs">
+                    {{ match.gGymnasiumName }}
+                </span>
+            </div>
+
+            <div id="score" class="flex w-fit text-black">
+                <div class="min-w-[20px] mr-1 text-right">
+                    {{ match.gHomeGoals }}
+                </div>
+                :
+                <div class="min-w-[20px] ml-1">
+                    {{ match.gGuestGoals }}
+                </div>
+            </div>
+            <div
+                id="info"
+                class="col-span-3 text-xs text-gray-600"
+                :class="match.gComment.length > 1 ? 'flex' : 'hidden'"
+            >
+                <InformationCircleIcon class="h-4 w-4 mr-1" />
+                {{ match.gComment }}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import {
+    ClockIcon,
+    LocationMarkerIcon,
+    InformationCircleIcon,
+} from "@heroicons/vue/solid";
+
+
+
+const props = defineProps(['match']);
+
+</script>
