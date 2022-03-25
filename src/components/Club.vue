29141996be37700db7ce55e29251e82d7076490f
Paul Paffe <paul.paffe@gmx.de>
2022-03-25 20:08:21 +0100

    added helper components to club and team

--- src/components/Club.vue
+++ src/components/Club.vue
@@ -116,82 +116,24 @@
                             v-show="teamID && !teamLoading"
                             class="ml-auto mr-0 w-fit block underline-offset-2 underline hover:text-indigo-700 text-indigo-900"
                             >Zum Team</router-link
-                        
                         >
-                        <span v-show="teamLoading" class="h-4 w-14 ml-auto mr-0 bg-indigo-300 rounded block underline-offset-2 underline hover:text-indigo-700 text-indigo-900"></span>
+                        <span
+                            v-show="teamLoading"
+                            class="h-4 w-14 ml-auto mr-0 bg-indigo-300 rounded block underline-offset-2 underline hover:text-indigo-700 text-indigo-900"
+                        ></span>
                         <div v-if="teamLoading">
-                            <div
-                                v-for="n in 3"
-                                :key="n"
-                                class="rounded-lg  h-[88px] w-full bg-indigo-200 my-1 p-2 justify-between px-5 hover:scale-95 scale-100 transition-all"
-                            >
-                                <div
-                                    class="grid grid-cols-3 sm:grid-cols-6 gap-1"
-                                >
-                                    <div
-                                        id="teams"
-                                        class=" text-black w-fit col-span-2 text-xs font-bold"
-                                    >
-                                        <!-- teams -->
-                                        <span class="w-12 h-4 animate-pulse rounded -mb-1 bg-gray-500 inline-block"></span> :
-                                        <span class="w-12 h-4 animate-pulse rounded -mb-1 bg-gray-500 inline-block"></span>
-                                    </div>
-
-                                    <div
-                                        id="date-time"
-                                        class="w-fit text-xs text-gray-800"
-                                    >   
-                                        <span class="inline-block w-6 animate-pulse rounded h-3 bg-gray-500"></span>
-                                        <div class="flex">
-                                            <span class="w-14 animate-pulse rounded h-3 bg-gray-500"></span>
-                                        </div>
-                                        <div class="flex">
-                                            <div
-                                            class="h-4 w-4 mt-0.5 animate-pulse bg-gray-500 rounded-full mr-1 shrink-0"
-                                        ></div>
-                                            <span class="w-8 h-3 mt-1  animate-pulse rounded m-0.5 bg-gray-500"></span>
-                                        </div>
-                                    </div>
-
-                                    <div
-                                        id="location"
-                                        class="flex text-black col-span-2 mt-1"
-                                    >
-                                        <!-- Gymnasium -->
-                                        <div
-                                            class="h-4 w-4 bg-gray-500 animate-pulse rounded-full mr-1 shrink-0"
-                                        ></div>
-                                        <span class="truncate text-xs">
-                                            <span class="w-20 rounded animate-pulse h-4 bg-gray-500 inline-block"></span>
-                                        </span>
-                                    </div>
-
-                                    <div
-                                        id="score"
-                                        class="flex w-fit text-black"
-                                    >
-                                        <div
-                                            class="min-w-[20px] mr-1 text-right"
-                                        >
-                                            <span class="w-6 animate-pulse -mb-1 h-4 inline-block rounded bg-gray-500"></span>
-                                        </div>
-                                        :
-                                        <div class="min-w-[20px] ml-1">
-                                            <span class="w-6 animate-pulse -mb-1 h-4 inline-block rounded bg-gray-500"></span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                            <MatchLoading v-for="n in 3" :key="n">
+                            </MatchLoading>
                         </div>
                         <div v-else>
                             <div id="league-info">
                                 <!-- Information about how many games and button to team component -->
                             </div>
-                            <MatchVue
+                            <Match
                                 v-for="match in teamMatches"
                                 :key="match.gID"
                                 :match="match"
-                            />
+                            ></Match>
                             <div id="no-data" v-show="!teamMatches.length">
                                 <div id="no-future-matches" v-show="!showAll">
                                     Keine zukünftigen Spiele
@@ -200,7 +142,6 @@
                                     Keine Spiele in dieser Klasse
                                 </div>
                             </div>
-                            
                         </div>
                     </DisclosurePanel>
                     <button
@@ -230,7 +171,8 @@ import {
 import { StarIcon as StarIconOutline, ShareIcon } from "@heroicons/vue/outline";
 import { Disclosure, DisclosureButton, DisclosurePanel } from "@headlessui/vue";
 
-import MatchVue from "./Match.vue";
+import Match from "./helpers/Match.vue";
+import MatchLoading from "./helpers/MatchLoading.vue";
 
 const route = useRoute();
 const club = ref({});
---
+++ src/components/helpers/MatchLoading.vue
@@ -0,0 +1,70 @@
+<template>
+    <div
+        class="rounded-lg h-[88px] w-full bg-indigo-200 my-1 p-2 justify-between px-5 hover:scale-95 scale-100 transition-all"
+    >
+        <div class="grid grid-cols-3 sm:grid-cols-6 gap-1">
+            <div
+                id="teams"
+                class="text-black w-fit col-span-2 text-xs font-bold"
+            >
+                <!-- teams -->
+                <span
+                    class="w-12 h-4 animate-pulse rounded -mb-1 bg-gray-500 inline-block"
+                ></span>
+                :
+                <span
+                    class="w-12 h-4 animate-pulse rounded -mb-1 bg-gray-500 inline-block"
+                ></span>
+            </div>
+
+            <div id="date-time" class="w-fit text-xs text-gray-800">
+                <span
+                    class="inline-block w-6 animate-pulse rounded h-3 bg-gray-500"
+                ></span>
+                <div class="flex">
+                    <span
+                        class="w-14 animate-pulse rounded h-3 bg-gray-500"
+                    ></span>
+                </div>
+                <div class="flex">
+                    <div
+                        class="h-4 w-4 mt-0.5 animate-pulse bg-gray-500 rounded-full mr-1 shrink-0"
+                    ></div>
+                    <span
+                        class="w-8 h-3 mt-1 animate-pulse rounded m-0.5 bg-gray-500"
+                    ></span>
+                </div>
+            </div>
+
+            <div id="location" class="flex text-black col-span-2 mt-1">
+                <!-- Gymnasium -->
+                <div
+                    class="h-4 w-4 bg-gray-500 animate-pulse rounded-full mr-1 shrink-0"
+                ></div>
+                <span class="truncate text-xs">
+                    <span
+                        class="w-20 rounded animate-pulse h-4 bg-gray-500 inline-block"
+                    ></span>
+                </span>
+            </div>
+
+            <div id="score" class="flex w-fit text-black">
+                <div class="min-w-[20px] mr-1 text-right">
+                    <span
+                        class="w-6 animate-pulse -mb-1 h-4 inline-block rounded bg-gray-500"
+                    ></span>
+                </div>
+                :
+                <div class="min-w-[20px] ml-1">
+                    <span
+                        class="w-6 animate-pulse -mb-1 h-4 inline-block rounded bg-gray-500"
+                    ></span>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
