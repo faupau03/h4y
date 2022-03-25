908ed94ebd38df7954e4e6ad754c36ea74e6e396
Paul Paffe <paul.paffe@gmx.de>
2022-03-25 12:06:54 +0100

    dist folder

--- server.js
+++ server.js
@@ -3,28 +3,28 @@ const app = express();
 
 app.set("trust proxy", "172.18.0.10");
 
-app.use(express.static('html'));
+app.use(express.static('dist'));
 
 app.get('/favorites', (req, res) => {
-    res.sendFile(__dirname + '/html/index.html')
+    res.sendFile(__dirname + '/dist/index.html')
 })
 app.get('/search', (req, res) => {
-    res.sendFile(__dirname + '/html/index.html')
+    res.sendFile(__dirname + '/dist/index.html')
 })
 app.get('/leagues', (req, res) => {
-    res.sendFile(__dirname + '/html/index.html')
+    res.sendFile(__dirname + '/dist/index.html')
 })
 app.get('/team', (req, res) => {
-    res.sendFile(__dirname + '/html/index.html')
+    res.sendFile(__dirname + '/dist/index.html')
 })
 app.get('/match', (req, res) => {
-    res.sendFile(__dirname + '/html/index.html')
+    res.sendFile(__dirname + '/dist/index.html')
 })
 app.get('/club', (req, res) => {
-    res.sendFile(__dirname + '/html/index.html')
+    res.sendFile(__dirname + '/dist/index.html')
 })
 app.get('/gym', (req, res) => {
-    res.sendFile(__dirname + '/html/index.html')
+    res.sendFile(__dirname + '/dist/index.html')
 })
 
 
