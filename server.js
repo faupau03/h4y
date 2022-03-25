aca59140220b043379d5c7619a5b705192796361
Paul Paffe <paul.paffe@gmx.de>
2022-03-25 11:44:01 +0100

    express serve

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+
+app.set("trust proxy", "172.18.0.10");
+
+app.use(express.static('html'));
+
+app.get('/favorites', (req, res) => {
+    res.sendFile(__dirname + '/html/index.html')
+})
+app.get('/search', (req, res) => {
+    res.sendFile(__dirname + '/html/index.html')
+})
+app.get('/leagues', (req, res) => {
+    res.sendFile(__dirname + '/html/index.html')
+})
+app.get('/team', (req, res) => {
+    res.sendFile(__dirname + '/html/index.html')
+})
+app.get('/match', (req, res) => {
+    res.sendFile(__dirname + '/html/index.html')
+})
+app.get('/club', (req, res) => {
+    res.sendFile(__dirname + '/html/index.html')
+})
+app.get('/gym', (req, res) => {
+    res.sendFile(__dirname + '/html/index.html')
+})
+
+
+
+const port = 80;
+app.listen(port, () => {
+    console.log(`App listening on port ${port}'`);
+});
