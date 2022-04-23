a6cb5c285a312634414ae6a1f3099a4a453f778d
Paul Paffe <paul.paffe@gmx.net>
2022-04-23 20:47:07 +0200

    fix prerendering, settling on static metadata

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const puppeteer = require('puppeteer');
 const ua = require('useragent');
 const app = express();
-
+const ejs = require('ejs');
+const res = require('express/lib/response');
+app.set('view engine', 'ejs');
 app.set("trust proxy", "172.18.0.10");
 
 
@@ -20,35 +21,69 @@ const uAgentMiddleware = async (req, res, next) => {
     if (!isBot(req.headers['user-agent'])) {
         next ()
     } else {
-
-        try {
-            console.log("This is a bot");
-            console.log("URL", `${req.protocol}://${req.get('host')}${req.originalUrl}`);
-
-            const browser = await puppeteer.launch({
-              'args' : [
-                '--disable-gpu',
-                '--disable-dev-shm-usage',
-                '--no-sandbox',
-                '--disable-setuid-sandbox'
-              ]
-            })
-            const page = await browser.newPage();
-            await page.setUserAgent('Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36');
-            await page.goto(`${req.protocol}://${req.get('host')}${req.originalUrl}`, {
-                waitUntil: "networkidle0",
-            });
-            await page.waitForFunction('loading === false');
-            const html = await page.evaluate(() => {
-                return document.documentElement.innerHTML;
-            });
-            await browser.close();
-            console.log("HTML", html);
-            res.send(html);
-        } catch (err) {
-            console.log("Error", err);
-            res.send(err)
+        console.log("Bot detected");
+        if (req.path == "/match") {
+            await ejs
+                .renderFile(__dirname + '/dist/og/match.ejs', { name: req.hostname })
+                .then(output => res.send(output));
         }
+        else if (req.path == "/gym") {
+            await ejs
+                .renderFile(__dirname + '/dist/og/gym.ejs', { name: req.hostname })
+                .then(output => res.send(output));
+        }
+        else if (req.path == "/team") {
+            await ejs
+                .renderFile(__dirname + '/dist/og/team.ejs', { name: req.hostname })
+                .then(output => res.send(output));
+        }
+        else if (req.path == "/club") {
+            await ejs
+                .renderFile(__dirname + '/dist/og/club.ejs', { name: req.hostname })
+                .then(output => res.send(output));
+        }
+        else if (req.path == "/search") {
+            await ejs
+                .renderFile(__dirname + '/dist/og/search.ejs', { name: req.hostname })
+                .then(output => res.send(output));
+        }
+        else if (req.path == "/favorites") {
+            await ejs
+                .renderFile(__dirname + '/dist/og/favorites.ejs', { name: req.hostname })
+                .then(output => res.send(output));
+        }
+        else {
+            next ()
+        }
+    //     try {
+    //         console.log("This is a bot");
+    //         console.log("URL", `${req.protocol}://${req.get('host')}${req.originalUrl}`);
+
+    //         const browser = await puppeteer.launch({
+    //           'args' : [
+    //             '--disable-gpu',
+    //             '--disable-dev-shm-usage',
+    //             '--no-sandbox',
+    //             '--disable-setuid-sandbox'
+    //           ]
+    //         })
+    //         const page = await browser.newPage();
+    //         await page.setUserAgent('Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36');
+    //         await page.goto(`${req.protocol}://${req.get('host')}${req.originalUrl}`, {
+    //             waitUntil: "networkidle0",
+    //         });
+    //         await page.waitForSelector('#loading_bot');
+    //         const html = await page.evaluate(() => {
+    //             return document.documentElement.innerHTML;
+    //         });
+    //         await browser.close();
+    //         console.log("HTML", html);
+    //         res.send(html);
+    //     } catch (err) {
+    //         console.log("Error", err);
+    //         res.send(err)
+    //     }
+        
     }
 }
 
