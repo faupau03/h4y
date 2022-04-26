00853099f8b200b9d6edf448cb241a4b4447ad1e
Paul Paffe <paul.paffe@gmx.net>
2022-04-26 15:39:25 +0200

    fix cal function

--- src/components/helpers/Cal.vue
+++ src/components/helpers/Cal.vue
@@ -16,13 +16,38 @@ const download = ref(null);
 const createIcal = () => {
     const cal = ical();
 let dateEnd = getDate(props.game);
-dateEnd.setHours(dateEnd.getHours() + 1.5);
+dateEnd.setMinutes(dateEnd.getMinutes() + 90);
 const event = cal.createEvent({
     start: getDate(props.game),
     end: dateEnd,
     summary: 'Handball Spiel ' + props.game.gHomeTeam + ' - ' + props.game.gGuestTeam,
+
+    url: window.location.href,
 });
-download.value.href = "data:text/calendar;" + cal.toBlob();
+event.location({
+    title: props.game.gGymnasiumName,
+    address: props.game.gGymnasiumStreet + ', ' + props.game.gGymnasiumPostal + ', ' + props.game.gGymnasiumTown,
+});
+
+event.description({
+    plain: 'Handballspiel, ' + props.game.gHomeTeam + ' gegen ' + props.game.gGuestTeam + ' in  ' + props.game.gGymnasiumName + ' in ' + props.game.gGymnasiumTown + ' am ' + getDate(props.game).toLocaleString('de-DE', {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long',
+        year: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+    }) + ' Uhr',
+})
+
+cal.prodId({
+    company: 'h4y.paffnet.de',
+    product: 'Handball4You',
+    language: 'DE',
+})
+console.log(cal.toString());
+download.value.href = "data:text/calendar;charset=utf8," + encodeURIComponent(cal.toString());
+download.value.setAttribute('download', "Handballspiel.ics");
 download.value.click();
 }
 
