2e4c05760f3a9806b2f87542cf995e84e5081465
Paul Paffe <paul.paffe@gmx.net>
2022-07-13 23:05:19 +0200

    apply new styling to press text

--- src/components/helpers/PressText.vue
+++ src/components/helpers/PressText.vue
@@ -1,32 +1,25 @@
 <template>
     <!-- Press text div -->
-    <div
-        v-if="showText && pressText.robotext"
-        class="pressText fixed left-0 bottom-0 right-0 top-0 w-screen bg-gray-900 bg-opacity-25 z-30 flex justify-center items-center"
-    >
-        <div class="z-30 max-h-screen overflow-auto">
-            <div
-            class="rounded-lg shadow-2xl bg-white  z-30 p-5 sm:mx-20 mx-5 my-10"
-        >
-            <span>
-                <XCircleIcon
-                    @click="showText = false"
-                    class="h-8 bg-indigo-300 rounded p-1 hover:bg-indigo-500 ml-auto"
-                />
-            </span>
-            <span v-html="pressText.robotext.text"> </span>
-            <span v-html="pressText.robotext.footer"> </span>
+    <div v-if="showText && pressText.robotext"
+        class="pressText fixed left-0 bottom-0 right-0 top-0 w-screen bg-base-content bg-opacity-30 z-30 flex justify-center items-center">
+        <div class="z-30 max-h-screen overflow-auto flex justify-center">
+            <div class="grid overflow-auto my-10 lg:max-w-[50%] rounded-lg shadow-2xl bg-base-100 justify-right z-30 p-3 sm:mx-20 mx-5">
+                <button class="btn btn-ghost ml-auto">
+                    <XIcon @click="showText = false" class="h-6" />
+                </button>
+                <span v-html="pressText.robotext.text"> </span>
+                <span v-html="pressText.robotext.footer"> </span>
+            </div>
         </div>
-        </div>
-        
+
     </div>
 </template>
 
 <script setup>
-import { XCircleIcon } from "@heroicons/vue/outline";
+import { XIcon } from "@heroicons/vue/outline";
 import { ref, watch } from "vue";
 
-const props = defineProps(["getText","game_id"]);
+const props = defineProps(["getText", "game_id"]);
 
 const showText = ref(false);
 const pressText = ref({});
@@ -34,7 +27,7 @@ const pressText = ref({});
 const showPressText = async () => {
     const response = await fetch(
         "https://spo.handball4all.de/service/robotext/if_robotext.php?cmd=text&game=" +
-            props.game_id
+        props.game_id
     );
     const data = await response.json();
 
@@ -61,6 +54,7 @@ watch(
     font-size: 1.5rem !important;
     margin: 0.5em 0;
 }
+
 .pressText h3 {
     font-size: 1rem !important;
     font-weight: 700;
@@ -68,7 +62,8 @@ watch(
 }
 
 .pressText p:first-of-type {
-    background-color: rgb(224, 231, 255, 0.7);
+    background-color: hsl(var(--s));
+    color: hsl(var(--sc));
     padding: 0.5em;
     margin-top: 0.5em;
     margin-bottom: 0.5em;
