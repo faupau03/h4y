749c130423592fde1551e65678f33fe76afd8e82
faupau03 <paul.paffe@gmx.net>
2023-04-17 08:33:42 +0200

    fix maskable icon

--- vite.config.js
+++ vite.config.js
@@ -36,31 +36,36 @@ export default defineConfig({
             src: 'pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512-maskable.png',
+            sizes: '512x512',
+            type: 'image/png',
             purpose: 'any maskable',
           }
         ],
         screenshots: [
           {
-              src: "Ticker_lineup.jpeg",
-              type: "image/jpeg",
-              sizes: "621x1344"
+            src: "Ticker_lineup.jpeg",
+            type: "image/jpeg",
+            sizes: "621x1344"
           },
           {
-              src: "Leagues.jpeg",
-              type: "image/jpeg",
-              sizes: "621x1344"
+            src: "Leagues.jpeg",
+            type: "image/jpeg",
+            sizes: "621x1344"
           },
           {
-              src: "Favorites.jpeg",
-              type: "image/jpeg",
-              sizes: "621x1344"
+            src: "Favorites.jpeg",
+            type: "image/jpeg",
+            sizes: "621x1344"
           },
           {
-              src: "Search.jpeg",
-              type: "image/jpeg",
-              sizes: "621x1344"
+            src: "Search.jpeg",
+            type: "image/jpeg",
+            sizes: "621x1344"
           }
-      ],
+        ],
       }
     })
   ]
